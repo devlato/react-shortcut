8a68f6c08d5e21f83a18bde9e2b7e68d0d686d4a
Denis Tokarev <denis.t@canva.com>
2020-03-30 22:23:44 +1100

    Comment

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,8 @@
+/**
+ * This function converts input keys into an array of strings, one string per key combination/key sequence
+ *
+ * @param keys String, comma-separated string, or an array of string specifying key combinations/sequences
+ */
 export const normalizeShortcuts = (keys: string | string[]): Shortcuts =>
   Array.isArray(keys)
     ? keys.map(normalizeShortcuts).reduce<Shortcut[]>((acc, v) => acc.concat(v), [])
